c91a114f55c7d7ae1b98a79429d068c4d4c17d55
Pavel Kalugin <p.kalugin@paralect.com>
2023-06-23 09:01:25 +0300

    day 9: implemented cli game where you move head

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,104 @@
+extern crate termion;
 use std::fs;
+use std::fmt;
 
-fn main() {
-    let input = fs::read_to_string("src/input8.txt").unwrap();
-    let grid = input.lines().map(|line| {
-        line.chars().map(|char| char as u8 - '0' as u8).collect::<Vec<u8>>().into_boxed_slice()
-    }).collect::<Vec<Box<[u8]>>>().into_boxed_slice();
-    let n = grid.len();
-    let m = grid[0].len();
+
+// cool staff
+// 1. termion
+// 2. /r/n https://stackoverflow.com/questions/48494508/how-do-i-create-a-new-line-when-using-termion-in-raw-mode
+
+use termion::event::{Key, Event, MouseEvent};
+use termion::input::{TermRead, MouseTerminal};
+use termion::raw::IntoRawMode;
+use termion::raw::RawTerminal;
+use std::io::{Write, stdout, stdin};
+struct GameState {
+    head_i: i32,
+    head_j: i32,
+    tail_i: i32,
+    tail_j: i32,
+}
+
+impl GameState {
+    fn new() -> GameState {
+        GameState {
+            head_i: 0,
+            head_j: 0,
+            tail_i: 0,
+            tail_j: 0,
+        }
+    }
+
+    fn move_top(&mut self) {
+        self.head_i += 1;
+    }
+
+    fn move_right(&mut self) {
+        self.head_j += 1;
+    }
+
+    fn move_down(&mut self) {
+        self.head_i -= 1;
+    }
+
+    fn move_left(&mut self) {
+        self.head_j -= 1;
+    }
+}
+
+impl fmt::Display for GameState {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        for i in (0..10).rev() {
+            for j in 0..10 {
+                if i == self.head_i && j == self.head_j {
+                    write!(f, "H")?;
+                } else if i == self.tail_i && j == self.tail_j {
+                    write!(f, "T")?;
+                } else {
+                    write!(f, ".")?;
+                }
+            }
+            write!(f, "\r\n")?;
+        }
+        Result::Ok(())
+    }
+}
+
+fn main() { 
+    let input = fs::read_to_string("src/input9_test.txt").unwrap();
+
+    let mut game_state = GameState::new();
     
-    let mut highest_scenic_score = 0;
-    let mut tree_spot = [0, 0];
+    let stdin = stdin();
+    let mut stdout = MouseTerminal::from(stdout().into_raw_mode().unwrap());
 
+    write!(stdout, "{}{}Use wasd to move head around. q to exit\r\n", termion::clear::All, termion::cursor::Goto(1, 1)).unwrap();
+    print!("{}", game_state);
 
-    for i in 0..n {
-        for j in 0..m{
-            let top = i - (0..i).rfind(|k| grid[*k][j] >= grid[i][j]).unwrap_or(0);
-            let bot = (i+1..n).find(|k| grid[*k][j] >= grid[i][j]).unwrap_or(n - 1) - i;
-            let left = j - (0..j).rfind(|k| grid[i][*k] >= grid[i][j]).unwrap_or(0);
-            let right = (j+1..m).find(|k| grid[i][*k] >= grid[i][j]).unwrap_or(m - 1) - j;
-
-            let scenic_score = left * right * top * bot;
-
-            if scenic_score > highest_scenic_score {
-                highest_scenic_score = scenic_score;
-                tree_spot = [i, j];
-
-                println!("New highest scenic score: {}, at ({}, {}), with top: {}, bot: {}, left: {}, right: {}", scenic_score, i, j, top, bot, left, right);
-            }
+    for c in stdin.events() {
+        let evt = c.unwrap();
+        match evt {
+            Event::Key(Key::Char('q')) => break,
+            Event::Key(Key::Char('d')) =>  {
+                game_state.move_right();
+            },
+            Event::Key(Key::Char('a')) =>  {
+                game_state.move_left();
+            },
+            Event::Key(Key::Char('w')) =>  {
+                game_state.move_top();
+            },
+            Event::Key(Key::Char('s')) =>  {
+                game_state.move_down();
+            },
+            _ => {}
         }
+        write!(stdout, "{}{}Use wasd to move head around. q to exit\r\nd", termion::clear::All, termion::cursor::Goto(1, 1)).unwrap();
+        print!("{}", game_state);
     }
-
-    for i in 0..n {
-        for j in 0..m {
-            if [i, j] == tree_spot {
-                print!("\x1b[41m{}\x1b[0m", grid[i][j]);
-            } else {
-                print!("{}", grid[i][j]);
-            }
-        }
-        println!()
-    }
-
-    println!("Highest scenic score: {}", highest_scenic_score);
 }
