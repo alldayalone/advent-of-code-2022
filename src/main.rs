4660a792c9a3ef2402a5dd8700a5423c18f7aa3a
Pavel Kalugin <p.kalugin@paralect.com>
2023-06-27 10:31:19 +0300

    day 12, first attemp (test passed but final result wrong). dejkstra

--- src/main.rs
+++ src/main.rs
@@ -1,215 +1,70 @@
 use std::fs;  
 
-#[derive(Clone)]
-struct Monkey {
-    name: String,
-    items: Vec<u64>,
-    increase_worry_level: fn(&u64) -> u64,
-    inspect_item: fn(&u64) -> usize,
+fn find_point(grid: &Vec<Vec<char>>, point: char) -> (usize, usize) {
+    let row = grid.iter().position(|row| row.contains(&point)).unwrap();
+    let col = grid.iter().find(|row| row.contains(&point)).unwrap().iter().position(|&x| x == point).unwrap();
+    (row, col)
 }
 
+fn find_neighbors(grid: &Vec<Vec<char>>, point: (usize, usize)) -> Vec<(usize, usize)> {
+    let mut neighbors: Vec<(usize, usize)> = Vec::new();
+    let (row, col) = point;
+    if row > 0  {
+        neighbors.push((row - 1, col));
+    }
+    if row < grid.len() - 1 {
+        neighbors.push((row + 1, col));
+    }
+    if col > 0 {
+        neighbors.push((row, col - 1));
+    }
+    if col < grid[0].len() - 1 {
+        neighbors.push((row, col + 1));
+    }
+    neighbors.into_iter().filter(|(row, col)| grid[*row][*col] <= (grid[point.0][point.1] as u8 + 1) as char).collect()
+}
 
 fn main() { 
-    // let input = fs::read_to_string("src/input10.txt").unwrap();
-    let mut monkeys: Vec<Monkey> = Vec::new();
-    
-    // input.lines().collect::<Vec<_>>().split(|line| line == &"\n").for_each(|lines| {
-    //     let mut iter = lines.iter();
+    let mut heightmap: Vec<Vec<char>> = fs::read_to_string("src/input12.txt").unwrap().lines().map(|line| line.chars().collect()).collect();
+    // let mut dirmap: Vec<Vec<char>> = heightmap.iter().map(|row| row.iter().map(|_| '.').collect()).collect();
+    let mut distmap: Vec<Vec<Option<u32>>> = heightmap.iter().map(|row| row.iter().map(|_| None).collect()).collect();
 
-    //     let name = iter.next().unwrap().to_string().replace(":", "").to_lowercase();
-    //     let items = iter.next().unwrap().split_at(18).1.split(", ").map(|item| {
-    //         item.parse::<u64>().unwrap()
-    //     }).collect::<Vec<_>>();
+    let start_point = find_point(&heightmap, 'S');
+    let end_point = find_point(&heightmap, 'E');
 
-    //     let operation = match iter.next().unwrap() {
-    //         "Starting"
-    //     };
-    //     let test = || { }; // iter.next();
-            
-    //     Monkey::new(name, items, operation, test)
-    // }).collect::<Vec<_>>();
+    heightmap[start_point.0][start_point.1] = 'a';
+    heightmap[end_point.0][end_point.1] = 'z';
 
+    // dirmap[end_point.0][end_point.1] = 'E';
+    distmap[start_point.0][start_point.1] = Some(0);
 
-    // monkeys.push(Monkey {
-    //     name: "Monkey 0".to_string(),
-    //     items: vec![79, 98],
-    //     increase_worry_level: |worry_level| {
-    //         return worry_level * 19
-    //     },
-    //     inspect_item: |item| {
-    //         if item % 23 == 0 { 2 } else { 3 }
-    //     },
-    // });
+    let mut queue: Vec<(usize, usize)> = vec![start_point.clone()];
 
-    // monkeys.push(Monkey {
-    //     name: "Monkey 1".to_string(),
-    //     items: vec![54, 65, 75, 74],
-    //     increase_worry_level: |worry_level| {
-    //         return worry_level + 6
-    //     },
-    //     inspect_item: |item| {
-    //         if item % 19 == 0 { 2 } else { 0 }
-    //     },
-    // });
+    while let Some(current_point) = queue.pop() {
+        let neighbors = find_neighbors(&heightmap, current_point);
 
-    // monkeys.push(Monkey {
-    //     name: "Monkey 2".to_string(),
-    //     items: vec![79, 60, 97],
-    //     increase_worry_level: |worry_level| {
-    //         worry_level * worry_level
-    //     },
-    //     inspect_item: |item| {
-    //         if item % 13 == 0 { 1 } else { 3 }
-    //     },
-    // });
-
-    // monkeys.push(Monkey {
-    //     name: "Monkey 3".to_string(),
-    //     items: vec![74],
-    //     increase_worry_level: |worry_level| {
-    //         worry_level + 3
-    //     },
-    //     inspect_item: |item| {
-    //         if item % 17 == 0 { 0 } else { 1 }
-    //     },
-    // });
-
-        // prod data
-    monkeys.push(Monkey {
-        name: "Monkey 0".to_string(),
-        items: vec![66, 79],
-        increase_worry_level: |worry_level| {
-            worry_level * 11
-        },
-        inspect_item: |item| {
-            if item % 7 == 0 { 6 } else { 7 }
-        },
-    });
-
-    monkeys.push(Monkey {
-        name: "Monkey 1".to_string(),
-        items: vec![84, 94, 94, 81, 98, 75],
-        increase_worry_level: |worry_level| {
-            worry_level * 17
-        },
-        inspect_item: |item| {
-            if item % 13 == 0 { 5 } else { 2 }
-        },
-    });
-
-
-    monkeys.push(Monkey {
-        name: "Monkey 2".to_string(),
-        items: vec![85, 79, 59, 64, 79, 95, 67],
-        increase_worry_level: |worry_level| {
-            worry_level + 8
-        },
-        inspect_item: |item| {
-            if item % 5 == 0 { 4 } else { 5 }
-        },
-    });
-
-    monkeys.push(Monkey {
-        name: "Monkey 3".to_string(),
-        items: vec![70],
-        increase_worry_level: |worry_level| {
-            worry_level + 3
-        },
-        inspect_item: |item| {
-            if item % 19 == 0 { 6 } else { 0 }
-        },
-    });
-
-    monkeys.push(Monkey {
-        name: "Monkey 4".to_string(),
-        items: vec![57, 69, 78, 78],
-        increase_worry_level: |worry_level| {
-            worry_level + 4
-        },
-        inspect_item: |item| {
-            if item % 2 == 0 { 0 } else { 3 }
-        },
-    });
-
-    monkeys.push(Monkey {
-        name: "Monkey 5".to_string(),
-        items: vec![65, 92, 60, 74, 72],
-        increase_worry_level: |worry_level| {
-            worry_level + 7
-        },
-        inspect_item: |item| {
-            if item % 11 == 0 { 3 } else { 4 }
-        },
-    });
-
-    monkeys.push(Monkey {
-        name: "Monkey 6".to_string(),
-        items: vec![77, 91, 91],
-        increase_worry_level: |worry_level| {
-            worry_level * worry_level
-        },
-        inspect_item: |item| {
-            if item % 17 == 0 { 1 } else { 7 }
-        },
-    });
-
-    monkeys.push(Monkey {
-        name: "Monkey 7".to_string(),
-        items: vec![76, 58, 57, 55, 67, 77, 54, 99],
-        increase_worry_level: |worry_level| {
-            worry_level + 6
-        },
-        inspect_item: |item| {
-            if item % 3 == 0 { 2 } else { 1 }
-        },
-    });
-
-    let mut inspection_counts = [0;8];
-
-    for round in 1..=10000 {
-        (0..monkeys.len()).for_each(|monkey_index| {
-            let monkey = monkeys[monkey_index].clone();
-            // println!("{}:", monkey.name);
-
-            for item in monkey.items {
-                inspection_counts[monkey_index] += 1;
-                // println!(" Monkey inspects an item with a worry level of {}.", item);
-
-                let mut new_item = (monkey.increase_worry_level)(&item);
-                // println!("  Worry level is X to {}.", new_item);
-
-                new_item = new_item % (2 * 3 * 5 * 7 * 11 * 13 * 17 * 19);
-                // println!("  Monkey gets bored with item. Worry level is divided by 3 to {}.", new_item);
-                // println!("  Current worry level.");
-
-                let new_owner = (monkey.inspect_item)(&new_item);
-                monkeys[new_owner].items.push(new_item);
-                // println!("  Item with worry level {} is thrown to monkey {}.", new_item, new_owner);
-            }
-
-            monkeys[monkey_index].items.clear();
-        });
-
-        if (round % 1000 == 0) {
-            println!("== After round {} ==", round);
-            inspection_counts.iter().enumerate().for_each(|(index, count)| {
-                println!("Monkey {} inspected items {} items.", index, count);
-            });
-
-
-            // println!("\nAfter round {}, the monkeys are holding items with these worry levels:", round);
-            // monkeys.iter().for_each(|monkey| {
-            //     println!("{}: {:?}", monkey.name, monkey.items);
-            // });
-            println!();
+        for neighbor in neighbors {
+            match distmap[neighbor.0][neighbor.1] {
+                Some(dist) => {
+                    if dist > distmap[current_point.0][current_point.1].unwrap() + 1 {
+                        distmap[neighbor.0][neighbor.1] = Some(distmap[current_point.0][current_point.1].unwrap() + 1);
+                    }
+                }
+                None => {
+                    queue.push(neighbor.clone());
+                    distmap[neighbor.0][neighbor.1] = Some(distmap[current_point.0][current_point.1].unwrap() + 1);
+                }
+            }       
         }
     }
 
-    inspection_counts.sort();
 
+    println!("{:?}", distmap[end_point.0][end_point.1].unwrap());
 
-    let monkey_business_level = inspection_counts.iter().rev().take(2).product::<u64>();
+    distmap.iter().for_each(|row| println!("{:?}", row.iter().map(|x| x.unwrap_or(0)).collect::<Vec<u32>>()));
 
-    println!("\nMonkey business level over 20 rounds: {}", monkey_business_level);
+    // convert distmap to array of u8 separated by /n
+    let contents = distmap.iter().map(|row| row.iter().map(|x| x.unwrap_or(0).to_string()).collect::<Vec<_>>().join("\t")).collect::<Vec<_>>().join("\n");
 
+    fs::write("src/output.txt", contents);
 }
